7b5d486d97a397197c414c06b6c77cdac639fafa
Osmanov <tu201906019@aztu.local>
2021-12-27 09:42:27 +0400

    first commit

---
+++ Class/Class/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Class.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ Class/Class/Models/Servis.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class.Models
+{
+    public class Servis
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100), Required(ErrorMessage = "title yaz")]
+        public string Tittle { get; set; }
+        [Column(TypeName = "ntext"), Required]
+        public string Context { get; set; }
+        public DateTime CratedTime { get; set; }
+        [MaxLength(250)]
+        [NotMapped]
+        public string Image { get; set; }
+        [ForeignKey("Client")]
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+        [ForeignKey("Category"),Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
+
+
